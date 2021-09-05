eee122e90d10b1f6679abda80f3b4457c51cd83f
asereewit <68099601+asereewit@users.noreply.github.com>
2021-09-05 16:17:54 -0700

    Update swissprot_blastx_results_analysis.py

--- swissprot_blastx_results_analysis.py
+++ swissprot_blastx_results_analysis.py
@@ -3,7 +3,7 @@ import csv
 swissprot_hits_seqid=[]
 swissprot_hits_entries=[]
 with open('rubescens_transcriptome_ORF_swissprot_blastx_1bestalignment.csv', newline='') as csvfile:
-  csvreader=csv.reader(csvfile, delimiter='\t')
+  csvreader=csv.reader(csvfile)
   for row in csvreader:
     swissprot_hits_seqid.append(row[0])
     swissprot_hits_entries.append(row[1])
